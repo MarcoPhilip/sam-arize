b7beea8cce7cee9e2fc489fd8c405230f2b326f6
marcophilip <marcophilipagapito@gmail.com>
2025-08-07 19:37:36 -0700

    #38 Supplier Model created. subject for changes

--- main_app/models.py
+++ main_app/models.py
@@ -38,6 +38,11 @@ class Location(models.Model):
 
 # Supplier Model
 class Supplier(models.Model):
+    name = models.CharField(max_length=50)
+    contact_person = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    address = models.TextField() 
 
     def __str__(self):
         return self.name
