51c74e1a07ee6b3c51354f286a9fc75a443c037d
marcophilip <marcophilipagapito@gmail.com>
2025-08-07 18:50:06 -0700

    category, location and supplier models pre defined before the Asset model.

--- main_app/models.py
+++ main_app/models.py
@@ -19,23 +19,43 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+# Category Model
+class Category(models.Model):
 
+    def __str__(self):
+        return self.name
+
+# Location Model
+class Location(models.Model):
+
+    def __str__(self):
+        return self.name
+    
+
+# Supplier Model
+class Supplier(models.Model):
+
+    def __str__(self):
+        return self.name
+    
 # Asset Model
 class Asset(models.Model):
     STATUS_CHOICES = [
-        ()
+        ('available', 'Available'),
+        ('unavailable', 'Unavailable'),
+        ('discontinued', 'Discontinued')
     ]
 
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category)
 # Inventory Model
 
 
-# Category Model
 
 
-# Location Model
+
 
 
 # Purchase Order Model
 
 
-# Supplier Model
