e90b62ea4a7bc566c338b68567bf0a0b2d648408
marcophilip <marcophilipagapito@gmail.com>
2025-08-09 18:41:54 -0700

    #12 all models are registered in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Category, Location, Asset, Inventory, PurchaseOrder
 
 # Register your models here.
+
+# User
+admin.site.register(User)
+# Category
+admin.site.register(Category)
+# Location
+admin.site.register(Location)
+# Asset
+admin.site.register(Asset)
+# Inventory
+admin.site.register(Inventory)
+# PurchaseOrder
+admin.site.register(PurchaseOrder)
+
