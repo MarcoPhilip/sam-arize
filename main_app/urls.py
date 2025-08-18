ca7e4fa2e30dbd05a0c3a5b7ba3babc5a957fb64
nesaratai <nesaratai@gmail.com>
2025-08-18 19:13:35 -0400

    add inventory delet confirm.html and update the views form inventory add edit and delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import dashboard, home, SignupView
+from . import views
 
 urlpatterns = [
     # Auth
@@ -8,7 +9,10 @@ urlpatterns = [
     path('login/',  LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
-  
+    path('inventory/', views.inventory_list, name='inventory_list'),
+    path('inventory/add/', views.inventory_add, name='inventory_add'),
+    path('inventory/edit/<int:pk>/', views.inventory_edit, name='inventory_edit'),
+    path('inventory/delete/<int:pk>/', views.inventory_delete, name='inventory_delete'),
     # Example dashboard (protected)
 
     # TODO: Dashboard route needs to be protected. Home page('') route isnt protected. If logged in, send the user to dashboard. Else, send to home. 
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Inventory
+from .forms import InventoryForm
 
 User = get_user_model()
 
@@ -14,6 +16,43 @@ def home(request):
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+# List inventory
+def inventory_list(request):
+    inventory = Inventory.objects.all()
+    return render(request, 'inventory/inventory_list.html', {'inventory': inventory})
+
+# Add inventory
+def inventory_add(request):
+    if request.method == 'POST':
+        form = InventoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory_list')
+    else:
+        form = InventoryForm()
+    return render(request, 'inventory/inventory_add.html', {'form': form})
+
+# Edit inventory
+def inventory_edit(request, pk):
+    item = get_object_or_404(Inventory, pk=pk)
+    if request.method == 'POST':
+        form = InventoryForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory/inventory_list')
+    else:
+        form = InventoryForm(instance=item)
+    return render(request, 'inventory_edit.html', {'form': form})
+
+# Delete inventory
+def inventory_delete(request, pk):
+    item = get_object_or_404(Inventory, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('inventory_list')
+    return render(request, 'inventory/inventory_delete_confirm.html', {'item': item})
+
+
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
@@ -23,3 +62,4 @@ class SignupView(CreateView):
     template_name = "accounts/signup.html"
     form_class = SignupForm
     success_url = reverse_lazy("login")
+
