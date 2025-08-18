2e31343a0c226d0c060f5bdf223cc9490a7ac6df
nesaratai <nesaratai@gmail.com>
2025-08-18 18:24:47 -0400

    add edit and list inventory html

---
+++ main_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Inventory
+
+class InventoryForm(forms.ModelForm):
+    class Meta:
+        model = Inventory
+        fields = ['name', 'category', 'location', 'quantity', 'unit_price']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'location': forms.Select(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            'unit_price': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
